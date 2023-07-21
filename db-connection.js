bc0c00a76f6de407f970de0dd685aa131deedfe9
skdhameliya <smitdhameliya10@gmail.com>
2023-07-21 14:53:10 -0400

    changed db url

--- db-connection.js
+++ db-connection.js
@@ -1,6 +1,6 @@
 // smitdhameliya801
 const mongoose = require("mongoose")
-mongoose.connect("mongodb+srv://smitdhameliya801:smit801@cluster0.w5hjmll.mongodb.net/rider", {
+mongoose.connect("mongodb+srv://adminsmit:smit@cluster0.6qawk.mongodb.net/rider", {
     useNewUrlParser:true,
     useUnifiedTopology:true
 }).then(() => console.log("connection success...")).catch((error) => console.log(error))
